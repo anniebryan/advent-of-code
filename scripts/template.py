7f4d387045ae0dfc5da085537d3ac26d84ab9f94
Annie Bryan <anniebryan@alum.mit.edu>
2025-11-27 17:20:40 -0500

    add all useful imports to template

--- scripts/template.py
+++ scripts/template.py
@@ -3,10 +3,29 @@ Advent of Code $YEAR
 Day $DAY
 """
 
+import ast
+import copy
+import heapq
+import operator
 import os
+import re
+import string
+import sys
+from collections import Counter, defaultdict, deque
+from collections.abc import Iterator
+from enum import Enum
+from functools import cmp_to_key, lru_cache, reduce
+from itertools import combinations, cycle
+from math import lcm, prod
 from pathlib import Path
+from statistics import median
+from typing import Callable, Iterable, Iterator, Literal
 
 import click
+import numpy
+import regex
+
+from utils import DirectedGraph, Grid, IntRangeMap, IntRangeSet
 
 
 def parse_input(puzzle_input: list[str], part_2: bool):
