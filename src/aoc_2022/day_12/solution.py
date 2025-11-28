5a49570dc08fd42448c2e4062f0c31b981d0f607
Annie Bryan <anniebryan@alum.mit.edu>
2025-11-27 19:18:58 -0500

    remove script setup logic from solution.py files

--- src/aoc_2022/day_12/solution.py
+++ src/aoc_2022/day_12/solution.py
@@ -87,38 +87,3 @@ def solve_part_2(puzzle_input: list[str]):
         return "Unable to get to E from any square at elevation a"
     best_start_loc = min(d, key=d.get)
     return f"Starting from {best_start_loc} gives a min distance of {d[best_start_loc]}"
-
-
-@click.command()
-@click.option("-se", "--skip_example", is_flag=True, default=False)
-@click.option("-sp", "--skip_puzzle", is_flag=True, default=False)
-def main(skip_example: bool = False, skip_puzzle: bool = False) -> None:
-    base_dir = Path(__file__).parent
-    example_files = sorted([fn for fn in os.listdir(base_dir) if fn.endswith(".txt") and "example" in fn])
-
-    def _run_solution(filename: str, display_name: str):
-        print(f"--- {display_name} ---")
-
-        if not (filepath := (base_dir / filename)).exists():
-            print(f"{filename} not found.")
-            return
-
-        with open(filepath) as file:
-            puzzle_input = [line.strip("\n") for line in file]
-            print(f"Part 1: {solve_part_1(puzzle_input)}")
-            print(f"Part 2: {solve_part_2(puzzle_input)}")
-        return
-
-    if not skip_example:
-        if len(example_files) < 2:
-            _run_solution("example.txt", "Example")
-        else:
-            for i, filename in enumerate(example_files):
-                _run_solution(filename, f"Example {i + 1}")
-
-    if not skip_puzzle:
-        _run_solution("puzzle.txt", "Puzzle")
-
-
-if __name__ == "__main__":
-    main()
