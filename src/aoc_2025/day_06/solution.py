e85fb9deada6f02529f6f3c28343782f7bb698a8
Annie Bryan <anniebryan@alum.mit.edu>
2025-12-06 15:48:53 -0500

    refactor 2025 day 6

--- src/aoc_2025/day_06/solution.py
+++ src/aoc_2025/day_06/solution.py
@@ -9,13 +9,36 @@ from math import prod
 
 def parse_input(puzzle_input: list[str], part_2: bool):
     if part_2:
-        problems = defaultdict(dict)
+        chars = defaultdict(dict)
         for i, line in enumerate(puzzle_input):
             for j, ch in enumerate(line[::-1]):
                 if ch.isnumeric():
-                    problems[j][i] = int(ch)
+                    chars[j][i] = int(ch)
                 else:
-                    problems[j][i] = ch
+                    chars[j][i] = ch
+
+        i = 0
+        problems = defaultdict(list)
+        for col in sorted(chars):
+            digits, op = [], None
+            for _, val in sorted(chars[col].items()):
+                if isinstance(val, int):
+                    digits.append(val)
+                elif val in {"+", "*"}:
+                    op = val
+                elif val == " ":
+                    pass
+                else:
+                    raise ValueError(f"Unexpected {val=}")
+            if digits:
+                num = 0
+                for d in digits:
+                    num *= 10
+                    num += d
+                problems[i].append(num)
+            if op:
+                problems[i].append(op)
+                i += 1
     else:
         problems = defaultdict(list)
         for line in puzzle_input:
@@ -27,8 +50,7 @@ def parse_input(puzzle_input: list[str], part_2: bool):
     return problems
 
 
-def solve_part_1(puzzle_input: list[str]):
-    problems = parse_input(puzzle_input, False)
+def _solve(problems: dict[int, list]) -> int:
     tot = 0
     for p in problems.values():
         op = p[-1]
@@ -41,35 +63,11 @@ def solve_part_1(puzzle_input: list[str]):
     return tot
 
 
+def solve_part_1(puzzle_input: list[str]):
+    problems = parse_input(puzzle_input, False)
+    return _solve(problems)
+
+
 def solve_part_2(puzzle_input: list[str]):
     problems = parse_input(puzzle_input, True)
-    tot = 0
-    vals_in_problem = []
-    for col in sorted(problems):
-        digits = []
-        op = None
-        for row in sorted(problems[col]):
-            val = problems[col][row]
-            if isinstance(val, int):
-                digits.append(val)
-            elif val in {"+", "*"}:
-                op = val
-            elif val == " ":
-                pass
-            else:
-                raise ValueError(f"Unexpected {val=}")
-        if digits:
-            num = 0
-            for d in digits:
-                num *= 10
-                num += d
-            vals_in_problem.append(num)
-        if op is not None:
-            if op == "+":
-                tot += sum(vals_in_problem)
-            elif op == "*":
-                tot += prod(vals_in_problem)
-            else:
-                raise ValueError(f"Unknown operator: {op}")
-            vals_in_problem = []
-    return tot
+    return _solve(problems)
