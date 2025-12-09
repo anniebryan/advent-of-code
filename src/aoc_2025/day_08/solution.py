6b15889dc53a58934f38d47dbfa1d99b4e095b80
Annie Bryan <anniebryan@alum.mit.edu>
2025-12-08 20:38:08 -0500

    speed up 2025 day 8

--- src/aoc_2025/day_08/solution.py
+++ src/aoc_2025/day_08/solution.py
@@ -18,7 +18,7 @@ def parse_input(puzzle_input: list[str], part_2: bool):
     return num_connections, coordinates
 
 
-def get_squared_dists(coordinates: list[tuple[int, int, int]]) -> dict[tuple[int, int], float]:
+def get_dists(coordinates: list[tuple[int, int, int]]) -> dict[int, tuple]:
     # (i, j) -> squared distance between coordinates[i] and coordinates[j]
     sq_dists = {}
     for i, (x1, y1, z1) in enumerate(coordinates):
@@ -26,48 +26,40 @@ def get_squared_dists(coordinates: list[tuple[int, int, int]]) -> dict[tuple[int
             if i >= j:
                 continue
             sq_dists[i, j] = (x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2
-    return sq_dists
+
+    sq_dist_to_pair = {}
+    for (i, j), d in sq_dists.items():
+        if d in sq_dist_to_pair:
+            raise ValueError(f"Multiple pairs of coordinates have the same distance {d}")
+        sq_dist_to_pair[d] = (coordinates[i], coordinates[j])
+    return sq_dist_to_pair
 
 
-# TODO: speedup (currently ~10s)
 def solve_part_1(puzzle_input: list[str]):
     num_connections, coordinates = parse_input(puzzle_input, False)
-    dists = get_squared_dists(coordinates)
-
-    connections = []
-    for _ in range(num_connections):
-        min_dist = min(dists.values())
-        for (i, j), d in dists.items():
-            x, y = coordinates[i], coordinates[j]
-            if d == min_dist:
-                connections.append((x, y))
-                del dists[i, j]
-                break
-
-    g = nx.Graph()
-    g.add_edges_from(connections)
-    circuits = list(nx.connected_components(g))
-    largest_3_circuits = sorted([len(c) for c in circuits], reverse=True)[:3]
-
-    return prod(largest_3_circuits)
-
-
-# TODO: speedup (currently ~80s)
-def solve_part_2(puzzle_input: list[str]):
-    _, coordinates = parse_input(puzzle_input, True)
-    dists = get_squared_dists(coordinates)
+    dists = get_dists(coordinates)
 
     g = nx.Graph()
     g.add_nodes_from(coordinates)
-    circuits = list(nx.connected_components(g))
-    while len(circuits) > 1:
-        min_dist = min(dists.values())
-        for (i, j), d in dists.items():
-            x, y = coordinates[i], coordinates[j]
-            if d == min_dist:
-                g.add_edge(x, y)
-                del dists[i, j]
-                break
-        circuits = list(nx.connected_components(g))
+
+    for i, (_, (x, y)) in enumerate(sorted(dists.items())):
+        g.add_edge(x, y)
+        if i == num_connections:
+            break
+
+    return prod(sorted([len(c) for c in nx.connected_components(g)], reverse=True)[:3])
+
+
+def solve_part_2(puzzle_input: list[str]):
+    _, coordinates = parse_input(puzzle_input, True)
+    dists = get_dists(coordinates)
+
+    g = nx.Graph()
+    g.add_nodes_from(coordinates)
+
+    for _, (x, y) in sorted(dists.items()):
+        g.add_edge(x, y)
+        if nx.number_connected_components(g) == 1:
+            break
 
     return x[0] * y[0]
