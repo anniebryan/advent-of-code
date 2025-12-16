83249c7e1b0f01d0f06119735757c6b9bfd80b24
Annie Bryan <anniebryan@alum.mit.edu>
2025-12-16 12:28:30 -0500

    solve 2025 day 9

--- src/aoc_2025/day_09/solution.py
+++ src/aoc_2025/day_09/solution.py
@@ -3,37 +3,28 @@ Advent of Code 2025
 Day 9: Movie Theater
 """
 
-import ast
-import copy
-import heapq
-import operator
-import os
-import re
-import string
-import sys
-from collections import Counter, defaultdict, deque
-from collections.abc import Iterator
-from enum import Enum
-from functools import cmp_to_key, lru_cache, reduce
-from itertools import combinations, cycle
-from math import lcm, prod
-from pathlib import Path
-from statistics import median
-from typing import Callable, Iterable, Iterator, Literal
-
-import click
-import numpy
-import regex
-
-from aoc_utils import DirectedGraph, DirectedWeightedGraph, Grid, IntRangeMap, IntRangeSet
-
-
 def parse_input(puzzle_input: list[str], part_2: bool):
     locs = []
     for line in puzzle_input:
         x, y = line.split(",")
         locs.append((int(x), int(y)))
     return locs
+
+
+def any_edges_intersect(
+        x1: int, y1: int,
+        x2: int, y2: int,
+        edges: set[tuple[int, int, int, int]],
+) -> bool:
+    min_x, max_x = min(x1, x2), max(x1, x2)
+    min_y, max_y = min(y1, y2), max(y1, y2)
+
+    for (e_x1, e_y1, e_x2, e_y2) in edges:
+        min_ex, max_ex = min(e_x1, e_x2), max(e_x1, e_x2)
+        min_ey, max_ey = min(e_y1, e_y2), max(e_y1, e_y2)
+        if (min_x < max_ex) and (max_x > min_ex) and (min_y < max_ey) and (max_y > min_ey):
+            return True
+    return False
 
 
 def solve_part_1(puzzle_input: list[str]):
@@ -46,32 +37,18 @@ def solve_part_1(puzzle_input: list[str]):
     return max_rect_size
 
 
-def get_start_point(tiles_at_y: dict[int, IntRangeSet]) -> tuple[int, int]:
-    for y, s in tiles_at_y.items():
-        if s.num_ranges == 2 and s.num_values == 2:
-            assert s.min_value is not None
-            x = s.min_value + 1
-            return (x, y)
-    assert False
-
-
 def solve_part_2(puzzle_input: list[str]):
     locs = parse_input(puzzle_input, True)
 
-    vert_edges = defaultdict(lambda: IntRangeSet())
-    horiz_edges = defaultdict(lambda: IntRangeSet())
-
+    edges = set()
     for (x1, y1), (x2, y2) in zip(locs, locs[1:] + [locs[0]]):
-        if x1 == x2:
-            vert_edges[x1].add_range(min(y1, y2), abs(y2 - y1))
-        elif y1 == y2:
-            horiz_edges[y1].add_range(min(x1, x2), abs(x2 - x1))
-        else:
-            raise ValueError(f"Received adjacent locs with different x's and different y's: {(x1, y1)} and {(x2, y2)}")
+        edges.add((x1, y1, x2, y2))
 
-    print(dict(horiz_edges))
-    print(dict(vert_edges))
-
-    # TODO solve part 2
-
-    return
+    max_rect_size = 0
+    for (x1, y1) in locs:
+        for (x2, y2) in locs:
+            rect_size = (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
+            if rect_size > max_rect_size:
+                if not any_edges_intersect(x1, y1, x2, y2, edges):
+                    max_rect_size = max(max_rect_size, rect_size)
+    return max_rect_size
