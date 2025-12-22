b02a1dda11591bc9cf2e5fa2b93a4ed0654d0854
Annie Bryan <anniebryan@alum.mit.edu>
2025-12-21 21:53:40 -0500

    2023 day 19 minor refactor

--- src/aoc_2023/day_19/solution.py
+++ src/aoc_2023/day_19/solution.py
@@ -6,7 +6,6 @@ Day 19: Aplenty
 import re
 from enum import Enum, auto
 from math import prod
-from typing import Any
 
 MIN_VALUE = 1
 MAX_VALUE = 4_000
@@ -24,6 +23,16 @@ class Outcome(Enum):
         raise ValueError(f"Invalid Outcome: {self}")
 
 
+def parse_outcome(outcome: str) -> "Outcome | Rule | str":
+    if "," in outcome:
+        return Rule(outcome)
+    if outcome == "A":
+        return Outcome.ACCEPT
+    if outcome == "R":
+        return Outcome.REJECT
+    return outcome
+
+
 class Rule:
     def __init__(self, rule: str):
         cond, rest = rule.split(":", 1)
@@ -35,24 +44,8 @@ class Rule:
         self.cond = lambda x: (x > self.threshold) if self.sign == ">" else (x < self.threshold)
 
         outcome_if_true, outcome_if_false = rest.split(",", 1)
-
-        if "," in outcome_if_true:
-            self.outcome_if_true = Rule(outcome_if_true)
-        elif outcome_if_true == "A":
-            self.outcome_if_true = Outcome.ACCEPT
-        elif outcome_if_true == "R":
-            self.outcome_if_true = Outcome.REJECT
-        else:
-            self.outcome_if_true = outcome_if_true
-
-        if "," in outcome_if_false:
-            self.outcome_if_false = Rule(outcome_if_false)
-        elif outcome_if_false == "A":
-            self.outcome_if_false = Outcome.ACCEPT
-        elif outcome_if_false == "R":
-            self.outcome_if_false = Outcome.REJECT
-        else:
-            self.outcome_if_false = outcome_if_false
+        self.outcome_if_true = parse_outcome(outcome_if_true)
+        self.outcome_if_false = parse_outcome(outcome_if_false)
     
     def __repr__(self) -> str:
         return (
@@ -61,7 +54,7 @@ class Rule:
             + f"{repr(self.outcome_if_false)})"
         )
 
-    def execute(self, **kwargs: Any) -> str | Outcome:
+    def execute(self, **kwargs: int) -> str | Outcome:
         """Returns either an outcome (accept/reject) or the name of the next workflow to run."""
         value = kwargs[self.varname]
         outcome = self.outcome_if_true if self.cond(value) else self.outcome_if_false
@@ -106,23 +99,12 @@ def part_is_accepted(part: dict[str, int], rules: dict[str, Rule], rule_name: st
 def num_combinations_accepted(
         rules: dict[str, Rule],
         rule: Rule,
-        x_min: int = MIN_VALUE,
-        x_max: int = MAX_VALUE,
-        m_min: int = MIN_VALUE,
-        m_max: int = MAX_VALUE,
-        a_min: int = MIN_VALUE,
-        a_max: int = MAX_VALUE,
-        s_min: int = MIN_VALUE,
-        s_max: int = MAX_VALUE,
+        **kwargs: int,
 ) -> int:
     num_combinations = 0
 
-    kwargs = dict(x_min=x_min, x_max=x_max, m_min=m_min, m_max=m_max, a_min=a_min, a_max=a_max, s_min=s_min, s_max=s_max)
-    min_vals = {"x": x_min, "m": m_min, "a": a_min, "s": s_min}
-    max_vals = {"x": x_max, "m": m_max, "a": a_max, "s": s_max}
-
-    min_val = min_vals[rule.varname]
-    max_val = max_vals[rule.varname]
+    min_val = kwargs[f"{rule.varname}_min"]
+    max_val = kwargs[f"{rule.varname}_max"]
 
     if rule.outcome_if_true != Outcome.REJECT:
         if rule.sign == "<":
@@ -137,7 +119,10 @@ def num_combinations_accepted(
 
         if n > 0:
             if rule.outcome_if_true == Outcome.ACCEPT:
-                num_combinations += n * prod(max(max_vals[v] - min_vals[v] + 1, 0) for v in "xmas" if v != rule.varname)
+                num_combinations += n * prod(
+                    max(kwargs[f"{v}_max"] - kwargs[f"{v}_min"] + 1, 0)
+                    for v in "xmas" if v != rule.varname
+                )
             elif isinstance(rule.outcome_if_true, Rule):
                 num_combinations += num_combinations_accepted(rules, rule.outcome_if_true, **kwargs)
             else:
@@ -156,7 +141,10 @@ def num_combinations_accepted(
 
         if n > 0:
             if rule.outcome_if_false == Outcome.ACCEPT:
-                num_combinations += n * prod(max(max_vals[v] - min_vals[v] + 1, 0) for v in "xmas" if v != rule.varname)
+                num_combinations += n * prod(
+                    max(kwargs[f"{v}_max"] - kwargs[f"{v}_min"] + 1, 0)
+                    for v in "xmas" if v != rule.varname
+                )
             elif isinstance(rule.outcome_if_false, Rule):
                 num_combinations += num_combinations_accepted(rules, rule.outcome_if_false, **kwargs)
             elif rule.outcome_if_false != Outcome.REJECT:
@@ -176,4 +164,10 @@ def solve_part_1(puzzle_input: list[str]):
 
 def solve_part_2(puzzle_input: list[str]):
     rules, _ = parse_input(puzzle_input, True)
-    return num_combinations_accepted(rules, rules["in"])
+    return num_combinations_accepted(
+        rules, rules["in"],
+        x_min=MIN_VALUE, x_max=MAX_VALUE,
+        m_min=MIN_VALUE, m_max=MAX_VALUE,
+        a_min=MIN_VALUE, a_max=MAX_VALUE,
+        s_min=MIN_VALUE, s_max=MAX_VALUE,
+    )
