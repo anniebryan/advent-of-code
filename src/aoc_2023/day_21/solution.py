3b38bd90640fdb6859c817603dd556d452f8dd0b
Annie Bryan <anniebryan@alum.mit.edu>
2025-12-28 15:10:35 -0500

    solve 2023 day 21

--- src/aoc_2023/day_21/solution.py
+++ src/aoc_2023/day_21/solution.py
@@ -23,5 +23,28 @@ def solve_part_1(puzzle_input: list[str]):
 
 
 def solve_part_2(puzzle_input: list[str]):
-    _ = parse_input(puzzle_input, True)
-    raise NotImplementedError
+    grid = parse_input(puzzle_input, True)
+    start = grid.where("S")[0]
+    if grid.width != grid.height:
+        raise NotImplementedError("Solution does not work for non-square grids")
+
+    NUM_STEPS = 26501365
+    dist_to_border = grid.width // 2
+    remaining_steps = NUM_STEPS - dist_to_border
+    num_plots = remaining_steps // grid.width
+    if remaining_steps % num_plots != 0:
+        raise NotImplementedError("Solution does not work when number of steps is not whole number of plots")
+
+    # Note: the number of plots we can reach happens to follow a quadratic curve
+    num_reachable = {}
+    for num_grids in range(3):
+        max_path_length = num_grids * grid.width + grid.width // 2
+        dists = grid.dijkstra(start, max_path_length=max_path_length, allow_wrap_around=True)
+        num_reachable[num_grids] = len([v for v in dists.values() if v % 2 == max_path_length % 2])
+    n0, n1, n2 = num_reachable[0], num_reachable[1], num_reachable[2]
+
+    a = (n2 - 2 * n1 + n0) // 2
+    b = n1 - n0 - a
+    c = n0
+
+    return a * num_plots ** 2 + b * num_plots + c
